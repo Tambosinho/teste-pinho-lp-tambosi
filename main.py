fe6e3aa1b484f573962c870ff7fcf0cda0163a47
Joao Felipe Vilas Boas <127103088+Vilasz@users.noreply.github.com>
2023-08-16 10:21:40 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,27 +5,25 @@ Created on Wed Aug 16 07:59:55 2023
 @author: joaod
 """
 
-import q6, q7, questoes_lp
+import q6, q7, questoes_LP
 
 
 print("Questão 1:")
-questoes_lp.q1()
+print(questoes_LP.q1())
 
 print("Questão 2:")
-questoes_lp.q2()
+print(questoes_LP.q2())
 
 print("Questão 3:")
-questoes_lp.q3()
 
 print("Questão 4:")
-arrays = questoes_lp.q4()
+questoes_lp.q4()
 
 print("Questão 5:")
-questoes_lp.q5(arrays)
+questoes_lp.q5()
 
 print("Questão 6:")
 q6.questao_6()
 
 print("Questão 7:")
-q7.questao_7()
-
+print (q7.questao_7())
