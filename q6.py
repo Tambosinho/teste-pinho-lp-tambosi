5041638641cb1ffdb201963aba5477c42fae9a3f
Joao Pedro Tambosi <joaodupe2@gmail.com>
2023-08-16 08:11:48 -0300

    addei a qst 6

---
+++ q6.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 16 08:02:00 2023
+
+@author: joaod
+"""
+
+import math
+import numpy as np
+import numpy.random as npr
+
+
+def questao_6(a,b):
+     
+    a = np.array([1,2,3])
+    b = np.array([5,6,7])
+    print(np.cross(a,b))
+    
+
