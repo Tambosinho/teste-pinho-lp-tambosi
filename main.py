1c5586bb42d3f428617519aaa11fb0935847b544
Joao Pedro Tambosi <joaodupe2@gmail.com>
2023-08-16 09:02:55 -0300

    atualizei a main

--- main.py
+++ main.py
@@ -5,4 +5,19 @@ Created on Wed Aug 16 07:59:55 2023
 @author: joaod
 """
 
+import q6, q7, questoes_lp
+
+print("Questão 3:")
+
+print("Questão 4:")
+questoes_lp.q4()
+
+print("Questão 5:")
+questoes_lp.q5()
+
+print("Questão 6:")
+q6.questao_6()
+
+print("Questão 7:")
+q7.questao_7()
 
